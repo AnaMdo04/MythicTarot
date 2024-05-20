0ca900adae39035423d0e5863d9b7f6b0789c3a1
Ana Maria Montes de Ory <ana.montes@controlnet.local>
2024-05-20 14:44:57 +0200

    Página Inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,41 +3,49 @@
 @section('title', 'Inicio - MythicTarot')
 
 @section('content')
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('css/welcome.css') }}" rel="stylesheet"> <!-- Asegúrate de que esta línea esté presente -->
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="banner-area">
-                <div class="banner">Encabezado</div>
-            </div>
+    <div class="row">
+        <div class="col-12 text-center my-3">
         </div>
     </div>
 
-    <div class="row mt-5">
-        <div class="col-md-8">
+    <div class="row my-3">
+        <div class="col-12">
+            <div class="content-block large">  
+                <div class="content-block picture-small">
+                </div>
+        </div></div>
+        </div>
+    </div>
+
+    <div class="row my-3">
+        <div class="col-md-6">
+            <div class="content-block medium"></div>
+        </div>
+        <div class="col-md-6">
+            <div class="content-block medium"></div>
+        </div>
+    </div>
+
+    <div class="row my-3">
+        <div class="col-12">
             <div class="content-block large"></div>
+        </div>
+    </div>
+
+    <div class="row my-3">
+        <div class="col-md-4">
+            <div class="content-block small"></div>
         </div>
         <div class="col-md-4">
             <div class="content-block small"></div>
-            <div class="content-block small mt-3"></div>
-        </div>
-    </div>
-
-    <div class="row mt-5">
-        <div class="col-md-12">
-            <div class="content-block large"></div>
-        </div>
-    </div>
-
-    <div class="row mt-5">
-        <div class="col-md-4">
-            <div class="opinion">Opinión</div>
         </div>
         <div class="col-md-4">
-            <div class="opinion">Opinión</div>
-        </div>
-        <div class="col-md-4">
-            <div class="opinion">Opinión</div>
+            <div class="content-block small"></div>
         </div>
     </div>
+    </div>
+</div>
+@endsection
