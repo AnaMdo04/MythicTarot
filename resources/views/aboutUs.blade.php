0a19f5d241478899775eaa61908c6acc4c539dc8
Ana Montes de Ory <amonory327@g.educaand.es>
2024-05-25 21:07:57 +0200

    Página Sobre Nosotros

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -15,10 +15,11 @@
         <div class="col-12">
             <div class="content-block large">
                 <div class="left-image-container">
-                    <img src="{{ asset('path/to/your/image.jpg') }}" alt="Imagen Presentación" class="img-fluid">
+                    <img src="{{ asset('css/nuestraMision.webp') }}" alt="Imagen Nuestra Misión" class="img-fluid">
                 </div>
                 <div class="right-content-container">
-                    <p>¡Bienvenido a MythicTarot!</p>
+                    <h1>Nuestra Misión</h1>
+                    <p>En MythicTarot, nuestro objetivo es conectar a las personas con la sabiduría ancestral del tarot, proporcionando lecturas precisas y personalizadas que ayuden a nuestros usuarios a encontrar guía y claridad en sus vidas diarias.</p>
                 </div>
             </div>
         </div>
@@ -28,10 +29,11 @@
         <div class="col-12">
             <div class="content-block large">
                 <div class="right-content-container">
-                    <p>¡Bienvenido a MythicTarot!</p>
-                </div>   
+                    <h2>Nuestra Visión</h2>
+                    <p>Aspiramos a ser la plataforma líder en lecturas de tarot digital, innovando constantemente en nuestras tecnologías para ofrecer experiencias enriquecedoras que fomenten el autoconocimiento y el crecimiento personal.</p>
+                </div>
                 <div class="left-image-container">
-                    <img src="{{ asset('path/to/your/image.jpg') }}" alt="Imagen Presentación" class="img-fluid">
+                    <img src="{{ asset('css/nuestraVision.webp') }}" alt="Imagen Nuestra Visión" class="img-fluid">
                 </div>
             </div>
         </div>
@@ -42,10 +44,25 @@
         <div class="col-12">
             <div class="content-block large">
                 <div class="left-image-container">
-                    <img src="{{ asset('path/to/your/image.jpg') }}" alt="Imagen Presentación" class="img-fluid">
+                    <img src="{{ asset('css/whyMythictarot.webp') }}" alt="Imagen MythicTarot" class="img-fluid">
                 </div>
                 <div class="right-content-container">
-                    <p>¡Bienvenido a MythicTarot!</p>
+                    <h2>¿Por qué MythicTarot?</h2>
+                    <p>MythicTarot nace de la pasión por el misticismo y la tecnología. Cada carta y lectura están diseñadas para que interactúes con los arquetipos y símbolos de una manera que resuene profundamente con tu vida personal y espiritual.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row my-3">
+        <div class="col-12">
+            <div class="content-block large">
+                <div class="right-content-container">
+                    <h2>Nuestro Equipo</h2>
+                    <p>Compuesto por expertos en tarot, desarrolladores, y artistas, nuestro equipo trabaja incansablemente para asegurar que cada experiencia en MythicTarot sea única y personal.</p>
+                </div>
+                <div class="left-image-container">
+                    <img src="{{ asset('css/nuestroEquipo.webp') }}" alt="Imagen PNuestro Equipo" class="img-fluid">
                 </div>
             </div>
         </div>
