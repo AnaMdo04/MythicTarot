3ad1834e140bb9282907457e6ab7c4ccdb52c1c6
Ana Montes de Ory <amonory327@g.educaand.es>
2024-06-12 21:01:04 +0200

    Arreglada linea borrada sin querer

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -79,7 +79,7 @@
         @yield('content')
     </div>
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0sG1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
     <footer class="footer-area">
         <div class="container text-center">
