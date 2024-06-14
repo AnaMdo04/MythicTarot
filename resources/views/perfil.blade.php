79f908f56f734016ab022aba2cff5a97e9976353
Ana Montes de Ory <amonory327@g.educaand.es>
2024-06-14 19:21:21 +0200

    Arreglado fallo código perfil e implementadas fotos faltantes de cartas predeterminadas

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -1,55 +1,28 @@
 @extends('auth.layouts')
 
 @section('content')
-<link href="{{ asset('css/ajustes.css') }}" rel="stylesheet">
+<link href="{{ asset('css/perfil.css') }}" rel="stylesheet">
 
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Ajustes de Configuración</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('perfil.update') }}">
-                        @csrf
-                        @method('PUT')
-
-                        <div class="form-group">
-                            <label for="name">Nombre de Usuario</label>
-                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name', Auth::user()->name) }}">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password">Nueva Contraseña</label>
-                            <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password_confirmation">Confirmar Contraseña</label>
-                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="profile_image">Foto de Perfil</label>
-                            <div class="custom-file-input-container">
-                                <input type="file" class="form-control-file @error('profile_image') is-invalid @enderror" id="profile_image" name="profile_image" onchange="document.getElementById('file-name').textContent = this.files[0].name">
-                                <label for="profile_image" class="custom-file-label">Seleccionar archivo</label>
-                                <span id="file-name" class="file-name">Ningún archivo seleccionado</span>
-                            </div>
-                        </div>
-
-                        <button type="submit" class="btn btn-primary mt-3">Guardar Cambios</button>
-                    </form>
-
-                    @if (Auth::user()->profile_image)
-                        <div class="delete-button-container mt-3">
-                            <form id="delete-profile-image-form" method="POST" action="{{ route('perfil.deleteProfileImage') }}">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Eliminar Foto de Perfil</button>
-                            </form>
-                        </div>
-                    @endif
+            <div class="d-flex justify-content-between align-items-center custom-card">
+                <div class="left-section">
+                    <div class="button-container">
+                        <a href="{{ route('ajustes') }}" class="btn btn-secondary btn-block mb-3 custom-button">Ajustes de Configuración</a>
+                        <a href="{{ route('lecturas') }}" class="btn btn-secondary btn-block mb-3 custom-button">Mis Lecturas</a>
+                        <a href="{{ route('mis-disenios') }}" class="btn btn-secondary btn-block custom-button">Mis Diseños</a>
+                    </div>
+                </div>
+                <div class="right-section text-center">
+                    <div class="profile-image-container mb-3">
+                        @if (Auth::user()->profile_image)
+                            <img src="{{ asset('storage/' . Auth::user()->profile_image) }}" class="rounded-circle profile-image" alt="Profile Image">
+                        @else
+                            <i class="fas fa-user-circle fa-10x default-profile-icon"></i>
+                        @endif
+                    </div>
+                    <a href="{{ route('perfil.edit') }}" class="btn btn-primary mt-3">Cambiar Foto Perfil</a>
                 </div>
             </div>
         </div>
